bdde106eb749a2d493c2f08d1c7a55f5c139f37d
CristianMorales1999 <cm9225064@gmail.com>
2024-06-13 19:33:14 -0500

    1. Agregando estilos CSS a layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,11 +7,20 @@
     <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-
     <style>
+        th{
+            background-color: rgb(1,0,140);
+            text-align: center;
+        }
+        a{
+            color: white;
+        }
         .activo a{
-            color:red;
-            text-decoration: underline;
+            color: white;
+            /*text-decoration: underline;*/
+        }
+        .activo{
+            background-color: black;
         }
     </style>
 
